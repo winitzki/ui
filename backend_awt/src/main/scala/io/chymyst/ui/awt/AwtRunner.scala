3e875b06c08d3d3c618da5dd28412f545ec7b6d4
sergei.winitzki <sergei.winitzki@workday.com>
2023-08-04 19:45:02 +0200

    wip better layout

--- backend_awt/src/main/scala/io/chymyst/ui/awt/AwtRunner.scala
+++ backend_awt/src/main/scala/io/chymyst/ui/awt/AwtRunner.scala
@@ -5,6 +5,7 @@ import io.chymyst.ui.elm.{LabelAlignment, View}
 import java.awt._
 import java.awt.event.{ActionEvent, WindowAdapter, WindowEvent}
 import java.util.concurrent.atomic.AtomicInteger
+import javax.swing.{Box, BoxLayout}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Future, Promise}
 import scala.util.Success
@@ -12,7 +13,7 @@ import scala.util.Success
 object AwtRunner {
   lazy val (frame, panel) = getFrameAndPanel()
 
-  def labelAlignment: LabelAlignment => Int = {
+  def toAwtLabelAlignment: LabelAlignment => Int = {
     case LabelAlignment.Left => Label.LEFT
     case LabelAlignment.Center => Label.CENTER
     case LabelAlignment.Right => Label.RIGHT
@@ -22,8 +23,9 @@ object AwtRunner {
     val f = new Frame()
     f.setTitle("AWT Runner example")
     f.setBounds(400, 400, 640, 480)
-    f.setLayout(new GridLayout(1, 1, 0, 0))
-    val p = new Panel(new GridLayout(1, 1, 5, 5))
+    f.setLayout(new BoxLayout(f, BoxLayout.X_AXIS))  // TODO better layout, or just use Swing instead of AWT.
+    val p = new Panel()
+    p.setLayout(new BoxLayout(p, BoxLayout.X_AXIS))
     p.setName("main panel")
     f.add(p)
     f.addWindowListener(new WindowAdapter() {
@@ -55,34 +57,36 @@ object AwtRunner {
       if (clearPanel) inPanel.removeAll() //else println(s"DEBUG: Not clearing panel for view $view")
       subview match {
         case View.Label(text, alignment) =>
-          inPanel.add(new Label(text, labelAlignment(alignment)))
+          inPanel.add(new Label(text, toAwtLabelAlignment(alignment)))
 
         case View.Button(text, event: E) =>
           val button = new Button(text)
-          button.addActionListener { (e: ActionEvent) =>
+          button.addActionListener { (_: ActionEvent) =>
             p.trySuccess(event)
           }
           inPanel.add(button)
 
         case View.TileH(left, right) =>
-          val n = new Panel(new GridLayout(1, 2, 5, 5))
+          val n = new Panel()
+          n.setLayout(new BoxLayout(n, BoxLayout.X_AXIS)) // We are using a Swing layout manager here, and it seems to be working.
           n.setName(s"Panel for $subview")
           //        n.setVisible(true) // Not necessary.
           inPanel.add(n)
-          first(
-            render(left, n, false),
-            render(right, n, false),
-          ).onComplete { case Success(e: E) => p.success(e) }
+          val f1 = render(left, n, false)
+          val f2 = render(right, n, false)
+          n.add(Box.createHorizontalGlue)
+          first(f1, f2).onComplete { case Success(e: E) => p.success(e) }
 
         case View.TileV(top, bottom) =>
-          val n = new Panel(new GridLayout(2, 1, 5, 5))
+          val n = new Panel()
+          n.setLayout(new BoxLayout(n, BoxLayout.Y_AXIS))
           n.setName(s"Panel for $subview")
           //        n.setVisible(true) // Not necessary.
           inPanel.add(n)
-          first(
-            render(top, n, false),
-            render(bottom, n, false),
-          ).onComplete { case Success(e: E) => p.success(e) }
+          val f1 = render(top, n, false)
+          val f2 = render(bottom, n, false)
+          n.add(Box.createVerticalGlue)
+          first(f1, f2).onComplete { case Success(e: E) => p.success(e) }
       }
       frame.setVisible(true) // Need to do this after any changes in layout or adding components. Otherwise nothing is shown and frame.isValid == false.
       //    }
