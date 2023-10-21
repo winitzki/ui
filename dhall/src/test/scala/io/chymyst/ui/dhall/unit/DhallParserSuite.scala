b90de4a165c11eb94257c5ffd219c3362c5988d9
sergei.winitzki <sergei.winitzki@workday.com>
2023-10-21 15:06:43 +0200

    wip

--- dhall/src/test/scala/io/chymyst/ui/dhall/unit/DhallParserSuite.scala
+++ dhall/src/test/scala/io/chymyst/ui/dhall/unit/DhallParserSuite.scala
@@ -1,12 +1,14 @@
 package io.chymyst.ui.dhall.unit
 
 import fastparse.Parsed
-import io.chymyst.ui.dhall.{CBOR, Parser}
+import io.chymyst.ui.dhall.{CBOR, Parser, Syntax}
 import munit.FunSuite
 import TestUtils._
+import io.chymyst.ui.dhall.Syntax.DhallFile
 
 import java.io.{File, FileInputStream}
-import scala.util.Try
+import java.nio.file.{Files, Paths}
+import scala.util.{Failure, Success, Try}
 import scala.util.chaining.scalaUtilChainingOps
 
 class DhallParserSuite extends FunSuite {
@@ -48,12 +50,31 @@ class DhallParserSuite extends FunSuite {
   }
 
   test("convert standard examples for successful parsing into CBOR") {
-    val results = testFilesForSuccess.map { file =>
-      val result = for {
+    val results = testFilesForSuccess.flatMap { file =>
+      val r: Option[Syntax.Expression] = Try(Parser.parseDhall(new FileInputStream(file))).toOption.flatMap {
+        case Parsed.Success(DhallFile(_, expr), _) => Some(expr)
+        case _ => None
+      }
+      val result = r.map { expr => Try(CBOR.exprToBytes(expr)) }
+      if (result.exists(_.isFailure)) println(s"Parsing or converting file ${file.getName} to CBOR failed: ${result.get.failed.get.getMessage}")
+      result
+    }
+    println(s"Success count: ${results.count(_.isSuccess)}\nFailure count: ${results.count(_.isFailure)}")
+  }
+
+  test("validate CBOR encoding for standard examples") {
+    val results = testFilesForSuccess.flatMap { file =>
+      val validationFile = file.getAbsolutePath.replace("A.dhall", "B.dhallb")
+      val cborValidationBytes = Files.readAllBytes(Paths.get(validationFile))
+      val result1 = for {
         Parsed.Success(dhallValue, _) <- Try(Parser.parseDhall(new FileInputStream(file)))
         bytes <- Try(CBOR.exprToBytes(dhallValue.value))
       } yield bytes
-      if (result.isFailure) println(s"Parsing or converting file ${file.getName} to CBOR failed: ${result.failed.get.getMessage}")
+      val result = result1.toOption.map { bytes =>
+          if(bytes sameElements cborValidationBytes) Success(bytes)
+          else Failure(new Exception(s"CBOR encoding differs"))
+      }
+      if (result.exists(_.isFailure)) println(s"CBOR validation failed for file ${file.getName}: ${result.get.failed.get.getMessage}")
       result
     }
     println(s"Success count: ${results.count(_.isSuccess)}\nFailure count: ${results.count(_.isFailure)}")
