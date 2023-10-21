351373c028450c4ddbb0d875c5a9dbfe7dd1f40f
sergei.winitzki <sergei.winitzki@workday.com>
2023-10-21 14:09:30 +0200

    adding cbor tests, failure count 116

--- dhall/src/main/scala/io/chymyst/ui/dhall/CBOR.scala
+++ dhall/src/main/scala/io/chymyst/ui/dhall/CBOR.scala
@@ -4,11 +4,13 @@ package io.chymyst.ui.dhall
 import com.upokecenter.cbor.{CBORObject, CBORType}
 import com.upokecenter.numbers.EInteger
 import io.chymyst.ui.dhall.Syntax.{Expression, Natural}
-import io.chymyst.ui.dhall.SyntaxConstants.VarName
+import io.chymyst.ui.dhall.SyntaxConstants.{ConstructorName, FieldName, VarName}
 
 import scala.annotation.tailrec
 import scala.collection.immutable.Seq
+import scala.jdk.CollectionConverters.MapHasAsJava
 
+// See https://github.com/dhall-lang/dhall-lang/blob/master/standard/binary.md
 object CBOR {
 
   def exprToBytes(e: Expression): Array[Byte] = toCbor2(e).EncodeToBytes()
@@ -31,7 +33,7 @@ object CBOR {
     if (index < BigInt(1).<<(64))
       CBORObject.FromObject(index)
     else
-      CBORObject.FromObject(EInteger.FromBytes(index.toByteArray, false))
+      CBORObject.FromObject(EInteger.FromBytes(index.toByteArray, false)) // TODO: Does this work correctly? Do we need to set littleEndian = true?
 
   private def makeArray(codes: Option[Int]*)(exprs: Expression*): CBORObject = makeArrayC(codes: _*)(exprs.map(e => toCbor2(e)): _*)
 
@@ -49,19 +51,31 @@ object CBOR {
     case Expression.Variable(VarName(name), index) => CBORObject.NewArray().Add(name).Add(naturalToCbor2(index))
     case Expression.Lambda(VarName(name), tipe, body) => makeArray(Some(1))(tipe, body)
     case Expression.Forall(VarName(name), tipe, body) => makeArray(Some(2))(tipe, body)
-    case Expression.Let(name, tipe, subst, body) => ???
+
+    case e@Expression.Let(_, _, _, _) =>
+      @tailrec def loop(acc: Seq[CBORObject], expr: Expression): Seq[CBORObject] = expr match {
+        case Expression.Let(VarName(name), tipe, subst, body) => loop((acc :+ CBORObject.FromObject(name)) ++ tipe.map(toCbor2).toSeq :+ toCbor2(subst), body)
+        case _ => acc ++ Seq(toCbor2(expr))
+      }
+
+      makeArrayC(Some(25))(loop(Seq(), e): _*)
+
     case Expression.If(cond, ifTrue, ifFalse) => makeArray(Some(14))(cond, ifTrue, ifFalse)
+
     case Expression.Merge(record, update, tipe) =>
       val args: Seq[Expression] = Seq(record, update) ++ tipe.toSeq
       makeArray(Some(6))(args: _*)
+
     case Expression.ToMap(data, tipe) =>
       val args: Seq[Expression] = Seq(data) ++ tipe.toSeq
       makeArray(Some(27))(args: _*)
+
     case Expression.EmptyList(Expression.Application(Expression.Builtin(SyntaxConstants.Builtin.List), tipe)) => makeArray(Some(4))(tipe)
     case Expression.EmptyList(tipe) => makeArray(Some(28))(tipe)
     case Expression.NonEmptyList(head, tail) => makeArray(Some(4), None)((head +: tail): _*)
     case Expression.Annotation(data, tipe) => makeArray(Some(26))(data, tipe)
     case Expression.Operator(lop, op, rop) => makeArray(Some(3), Some(op.cborCode))(lop, rop)
+
     case f@Expression.Application(_, _) =>
       @tailrec def loop(args: Seq[Expression], expr: Expression): Seq[Expression] = expr match {
         case Expression.Application(f, a) => loop(a +: args, f)
@@ -69,27 +83,43 @@ object CBOR {
       }
 
       makeArray(Some(0))(loop(Seq(), f): _*)
-    case Expression.Field(base, name) => ???
-    case Expression.ProjectByLabels(base, labels) => ???
-    case Expression.ProjectByType(base, by) => ???
+
+    case Expression.Field(base, FieldName(name)) => makeArrayC(Some(9))(toCbor2(base), CBORObject.FromObject(name))
+    case Expression.ProjectByLabels(base, labels) => makeArrayC(Some(10))(toCbor2(base) +: labels.map(label => CBORObject.FromObject(label.name)): _*)
+    case Expression.ProjectByType(base, by) => makeArrayC(Some(10))(toCbor2(base), makeArrayC()(toCbor2(by)))
     case Expression.Completion(base, target) => makeArray(Some(3), Some(13))(base, target)
     case Expression.Assert(data) => makeArray(Some(19))(data)
+
     case Expression.With(data, pathComponents, body) => ???
-    case Expression.DoubleLiteral(value) => ???
+
+    case Expression.DoubleLiteral(value) => CBORObject.FromObject(value) // TODO: verify that this works correctly.
     case Expression.NaturalLiteral(value) => makeArrayC(Some(15))(naturalToCbor2(value))
-    case Expression.IntegerLiteral(value) => ???
+    case Expression.IntegerLiteral(value) => makeArrayC(Some(16))(CBORObject.FromObject(value)) // TODO: verify that this works correctly.
     case Expression.TextLiteralNoInterp(value) => makeArrayC(Some(18))(CBORObject.FromObject(value))
+
     case Expression.TextLiteral(interpolations, trailing) =>
-      val objects: Seq[CBORObject] = interpolations.flatMap { case (head, tail) => Seq(CBORObject.FromObject(head), toCbor2(tail)) } :+ CBORObject.FromObject(trailing)
+      val objects: Seq[CBORObject] =
+        interpolations
+          .flatMap { case (head, tail) => Seq(CBORObject.FromObject(head), toCbor2(tail)) } :+ CBORObject.FromObject(trailing)
       makeArrayC(Some(18))(objects: _*)
-    case Expression.BytesLiteral(hex) => ???
+
+    case b@Expression.BytesLiteral(hex) => makeArrayC(Some(33))(CBORObject.FromObject(b.bytes))
     case Expression.DateLiteral(date) => ???
     case Expression.TimeLiteral(time) => ???
     case Expression.TimeZoneLiteral(tz) => ???
     case Expression.RecordType(defs) => ???
     case Expression.RecordLiteral(defs) => ???
-    case Expression.RawRecordLiteral(base, defs) => ???
-    case Expression.UnionType(defs) => ???
+
+    case e@Expression.RawRecordLiteral(_, _) => toCbor2(Expression.RecordLiteral.of(Seq(e))) // This is a type defined at parse time and should not occur here.
+
+    case Expression.UnionType(defs) =>
+      val dict: java.util.Map[String, CBORObject] = defs.map {
+        case (ConstructorName(name), Some(expr)) => (name, toCbor2(expr))
+        case (ConstructorName(name), None) => (name, CBORObject.Null)
+      }.toMap.asJava
+
+      makeArrayC(Some(11))(CBORObject.FromObject(dict))
+
     case Expression.ShowConstructor(data) => makeArray(Some(34))(data)
     case Expression.Import(importType, importMode, digest) => ???
     case Expression.KeywordSome(data) => makeArray(Some(5), None)(data)
--- dhall/src/test/scala/io/chymyst/ui/dhall/unit/DhallParserSuite.scala
+++ dhall/src/test/scala/io/chymyst/ui/dhall/unit/DhallParserSuite.scala
@@ -1,18 +1,29 @@
 package io.chymyst.ui.dhall.unit
 
 import fastparse.Parsed
-import io.chymyst.ui.dhall.Parser
+import io.chymyst.ui.dhall.{CBOR, Parser}
 import munit.FunSuite
 import TestUtils._
+
 import java.io.{File, FileInputStream}
 import scala.util.Try
 import scala.util.chaining.scalaUtilChainingOps
 
 class DhallParserSuite extends FunSuite {
 
-  test("load standard examples for successful parsing") {
-    val testFiles = getClass.getClassLoader.getResource("parser-succeed").getPath.pipe(new File(_)).listFiles.toSeq
-    val results = testFiles.filter(_.getName endsWith ".dhall").sortBy(_.getName).map { file =>
+  def enumerateResourceFiles(directory: String, filterBySuffix: Option[String] = None): Seq[File] =
+    getClass.getClassLoader.getResource(directory)
+      .getPath
+      .pipe(new File(_)).listFiles.toSeq
+      .filter(f => filterBySuffix.forall(suffix => f.getName.endsWith(suffix)))
+      .sortBy(_.getName)
+
+  def testFilesForSuccess = enumerateResourceFiles("parser-succeed", Some(".dhall"))
+
+  def testFilesForFailure = enumerateResourceFiles("parser-fail", Some(".dhall"))
+
+  test("parse standard examples for successful parsing") {
+    val results = testFilesForSuccess.map { file =>
       val result = Try {
         val Parsed.Success(dhallValue, _) = Parser.parseDhall(new FileInputStream(file))
         dhallValue
@@ -23,15 +34,26 @@ class DhallParserSuite extends FunSuite {
     println(s"Success count: ${results.count(_.isSuccess)}\nFailure count: ${results.count(_.isFailure)}")
   }
 
-  test("load standard examples for failed parsing") {
-    val testFiles = getClass.getClassLoader.getResource("parser-fail").getPath.pipe(new File(_)).listFiles.toSeq
-    val results = testFiles.filter(_.getName endsWith ".dhall").sortBy(_.getName).map { file =>
+  test("parse standard examples for failed parsing") {
+    val results = testFilesForFailure.map { file =>
       val result = Try {
         val Parsed.Success(result, _) = Parser.parseDhall(new FileInputStream(file))
         result
       }
 
       if (result.isSuccess) println(s"Parsing file ${file.getName} expecting failure. Result: unexpected success:\n\t\t\t${result.get}\n")
+      result
+    }
+    println(s"Success count: ${results.count(_.isSuccess)}\nFailure count: ${results.count(_.isFailure)}")
+  }
+
+  test("convert standard examples for successful parsing into CBOR") {
+    val results = testFilesForSuccess.map { file =>
+      val result = for {
+        Parsed.Success(dhallValue, _) <- Try(Parser.parseDhall(new FileInputStream(file)))
+        bytes <- Try(CBOR.exprToBytes(dhallValue.value))
+      } yield bytes
+      if (result.isFailure) println(s"Parsing or converting file ${file.getName} to CBOR failed: ${result.failed.get.getMessage}")
       result
     }
     println(s"Success count: ${results.count(_.isSuccess)}\nFailure count: ${results.count(_.isFailure)}")
