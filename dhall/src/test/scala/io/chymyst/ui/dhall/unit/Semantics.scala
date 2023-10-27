180aa647e8120d75a2f7e7bfabe008e699b1205b
sergei.winitzki <sergei.winitzki@workday.com>
2023-10-27 20:17:02 +0200

    alpha-normalize

--- dhall/src/test/scala/io/chymyst/ui/dhall/unit/Semantics.scala
+++ dhall/src/test/scala/io/chymyst/ui/dhall/unit/Semantics.scala
@@ -14,24 +14,84 @@ object Semantics {
     val d = if (positive) 1 else -1
     expr.scheme match {
       case Variable(name, index) => if (name != x || index < minIndex) expr else Variable(name, index + d)
+
       case Lambda(name, tipe, body) =>
         val newMinIndex = if (name != x) minIndex else minIndex + 1
         Lambda(name, shift(positive, x, minIndex, tipe), shift(positive, x, newMinIndex, body))
+
       case Forall(name, tipe, body) =>
         val newMinIndex = if (name != x) minIndex else minIndex + 1
         Forall(name, shift(positive, x, minIndex, tipe), shift(positive, x, newMinIndex, body))
+
       case Let(name, tipe, subst, body) =>
         val newMinIndex = if (name != x) minIndex else minIndex + 1
         Let(name, tipe.map(shift(positive, x, minIndex, _)), shift(positive, x, minIndex, subst), shift(positive, x, newMinIndex, body))
+
       case other => other.map(expression => shift(positive, x, minIndex, expression))
     }
   }
 
   // See https://github.com/dhall-lang/dhall-lang/blob/master/standard/substitution.md
-  def substitute(expr: Expression, x: Variable, body: Expression): Expression = ???
+  def substitute(expr: Expression, substVar: VarName, substIndex: Natural, substTarget: Expression): Expression = expr.scheme match {
+    case v@Variable(_, _) => if (v.name == substVar && v.index == substIndex) substTarget else expr
+
+    case Lambda(name, tipe, body) =>
+      val newIndex = if (name != substVar) substIndex else substIndex + 1
+      val newType = substitute(tipe, name, substIndex, substTarget)
+      val newTarget = shift(true, name, 0, substTarget)
+      val newBody = substitute(body, name, newIndex, newTarget)
+      Lambda(name, newType, newBody)
+
+    case Forall(name, tipe, body) =>
+      val newIndex = if (name != substVar) substIndex else substIndex + 1
+      val newType = substitute(tipe, name, substIndex, substTarget)
+      val newTarget = shift(true, name, 0, substTarget)
+      val newBody = substitute(body, name, newIndex, newTarget)
+      Forall(name, newType, newBody)
+
+    case Let(name, tipe, subst, body) =>
+      val newIndex = if (name != substVar) substIndex else substIndex + 1
+      val newType = tipe.map(substitute(_, name, substIndex, substTarget))
+      val newSubst = substitute(subst, name, substIndex, substTarget)
+      val newTarget = shift(true, name, 0, substTarget)
+      val newBody = substitute(body, name, newIndex, newTarget)
+      Let(name, newType, newSubst, newBody)
+
+    case other => other.map(expression => substitute(expression, substVar, substIndex, substTarget))
+  }
+
+  // TODO: can we reuse this VarName elsewhere instead of using the string "_"?
+  val underscore = VarName("_")
 
   // See https://github.com/dhall-lang/dhall-lang/blob/master/standard/alpha-normalization.md
-  def alphaNormalize(expr: Expression): Expression = ???
+  def alphaNormalize(expr: Expression): Expression = expr.scheme match {
+    case Variable(_, _) => expr
+
+    case Lambda(name, tipe, body) => if (name == underscore) expr.map(alphaNormalize) else {
+      val body1 = shift(true, underscore, 0, body)
+      val body2 = substitute(body1, name, 0, Variable(underscore, 0))
+      val body3 = shift(false, name, 0, body2)
+      Lambda(underscore, alphaNormalize(tipe), alphaNormalize(body3))
+    }
+
+    case Forall(name, tipe, body) => if (name == underscore) expr.map(alphaNormalize) else {
+      val body1 = shift(true, underscore, 0, body)
+      val body2 = substitute(body1, name, 0, Variable(underscore, 0))
+      val body3 = shift(false, name, 0, body2)
+      Forall(underscore, alphaNormalize(tipe), alphaNormalize(body3))
+    }
+
+    case Let(name, tipe, subst, body) => if (name == underscore) expr.map(alphaNormalize) else {
+      val body1 = shift(true, underscore, 0, body)
+      val body2 = substitute(body1, name, 0, Variable(underscore, 0))
+      val body3 = shift(false, name, 0, body2)
+      Let(underscore, tipe.map(alphaNormalize), alphaNormalize(subst), alphaNormalize(body3))
+    }
+
+    case Import(_, _, _) => throw new Exception(s"alphaNormalize($expr): Imports cannot be α-normalized")
+    
+    case other => other.map(alphaNormalize)
+  }
 
   // See https://github.com/dhall-lang/dhall-lang/blob/master/standard/beta-normalization.md
   def betaNormalize(expr: Expression): Expression = ???
